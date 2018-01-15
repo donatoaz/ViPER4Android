12b54d17c669dcca0a3b7c2adbef7b4ffb6426ef
Shady Squirrel <shady.squirrel@caramail.com>
2018-01-15 12:19:31 +0100

    SEPolicy fix for OnePlus (and possibly others)
    Thanks to @TheEvilCoder42 for this

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,5 +5,8 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+# these two should allow sepolicy=enforcing processing on most of devices
 magiskpolicy --live "allow audioserver audioserver_tmpfs file { read write execute }"
 magiskpolicy --live "allow mediaserver mediaserver_tmpfs file { read write execute }"
+# this one seems to be specific to OnePlus 5, possible many others.
+magiskpolicy --live "allow hal_audio_default hal_audio_default process { execmem }"
