9518b6d4a6e512b76a7c11a71727ed589b091fc0
Shady Squirrel <shady.squirrel@caramail.com>
2018-01-21 18:46:09 +0100

    SEPolicy fix for Mi A1, Pixel and probably other Oreo devices

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,8 +5,12 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
 # these two should allow sepolicy=enforcing processing on most of devices
 magiskpolicy --live "allow audioserver audioserver_tmpfs file { read write execute }"
 magiskpolicy --live "allow mediaserver mediaserver_tmpfs file { read write execute }"
-# this one seems to be specific to OnePlus 5, possible many others.
+# this one seems to be specific to OnePlus 5, possibly many others.
 magiskpolicy --live "allow hal_audio_default hal_audio_default process { execmem }"
+# this one is a fix for Xiaomi Mi A1 and for Pixels running stock Oreo.
+# probably fixes sepolicy for many other Oreo devices.
+magiskpolicy --live "allow hal_audio_default hal_audio_default_tmpfs file { execute }"
